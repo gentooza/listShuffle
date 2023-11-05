51302d52426a5a47d9d6e7ac6779816b01072945
gentooza <joa.cuellar@riseup.net>
2023-11-05 10:10:52 +0100

    added seed option, for being able to offer repeatibility

--- listShuffle.py
+++ listShuffle.py
@@ -17,8 +17,10 @@ You should have received a copy of the GNU General Public License
 along with listShuffle.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
-from random import shuffle
+from random import shuffle, seed
 import argparse
+import time
+import sys
 
 def ReorderList(myList):
     try:
@@ -27,17 +29,27 @@ def ReorderList(myList):
         print("Error, can't reorder the input list: ", *myList)
     return myList
 
+def GenerateSeed(actualSeed):
+    seedUsed = actualSeed
+    if (args.seed and args.seed > 0):
+        seedUsed = args.seed
+    seed(seedUsed)
+    return seedUsed
+
 def ProgramArguments():
     parser = argparse.ArgumentParser("randomShuffler", )
     parser.add_argument("input_list_file", help="A plain text file with a list of names in every line.", type=str)
     parser.add_argument("output_list_file", help="A plain text file with the same list of names randomly reordered.", type=str)
     parser.add_argument("-l", "--limit", help="Limit output to X number of lines. (Supply X value!)", type=int, required=False)
+    parser.add_argument("-s", "--seed", help="Force a seed value for shuffling", type=float, required=False)
     return parser.parse_args()
 
 args = ProgramArguments()
+seedUsed = time.time()
 try:
     inputFile =open(args.input_list_file, "r")
     lines = inputFile.readlines()
+    seedUsed = GenerateSeed(seedUsed)
     reorderedLines = ReorderList(lines)
     if (args.limit):
         reorderedLines = reorderedLines[:args.limit]
@@ -47,10 +59,11 @@ try:
         outputFile.close()
         inputFile.close()
         print("Done!")
-    except:
-        print("Error, can't open for writting the output file:", args.output_list_file)
-except:
-    print("Error, can't open input file:", args.input_list_file)
+        print("Seed used: ", seedUsed )
+    except Exception as e:
+        print("Error,({0}): {1}, can't open for writting the output file:".format(e.errno, e.strerror), args.output_list_file)
+except Exception as e:
+   print("Error,({0}): {1}, can't open input file:".format(e.errno, e.strerror), args.input_list_file)
 
 
 
